e3c1daae600f3be26ea78b428bdf48c68dbbe6b0
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-10-01 13:46:35 -0700

    Add boilerplate

---
+++ src/actions/index.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import { Dispatch } from 'redux'
+import { ActionTypes } from './types'
+
+interface Todo {
+  id: number
+  title: string
+  completed: boolean
+}
+
+interface FetchToDosAction {
+  type: ActionTypes.fetchTodos
+  payload: Todo[]
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos'
+
+export const fetchTodos = () => {
+  // When using redux-thunk for promises, we return a function.
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url)
+
+    dispatch<FetchToDosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    })
+  }
+}
